9debeeba30f83b0e6a713a566b9af5144dfb3dd1
Yusuf Pisan <pisan@uw.edu>
2017-10-04 14:29:33 -0700

    starting

---
+++ classexercises/week02/PrintOutLargest/PrintOutLargest.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class PrintOutLargest here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class PrintOutLargest
+{
+    public static void largestOf3(int a, int b, int c)
+    {
+    }
+    
+    public static void main()
+    {
+        largestOf3(10, 20, 30);
+        largestOf3(5, 3, 1);
+        largestOf3(1, 7, 2);
+    }
+}
