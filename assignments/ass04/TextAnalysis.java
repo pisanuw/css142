0c54f4bbc5668231c1ba8ff808157120a89f2f31
Yusuf Pisan <pisan@uw.edu>
2017-10-26 09:35:26 -0700

    Template

---
+++ assignments/ass04/TextAnalysis.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class TextAnalysis here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class TextAnalysis
+{
+    // provided as an example, should not need it
+    public static void printAlphabet()
+    {
+        for (char curr =  'a'; curr <= 'z'; curr++)
+        {
+            System.out.printf("%c ", curr);
+        }
+    }
+
+    // return the number of times c appears in the message
+    public static int countChar(String message, char c)
+    {
+        return 0;
+    }
+    // given a String, 
+    // checks how many times each letter appears and prints it
+    public static void analyzeLine(String message)
+    {
+    }
+    // gets input from the user, calls analyzeLine
+    public static void textAnalysis()
+    {
+
+    }
+    
+    public static void main(String[] args)
+    {
+        textAnalysis();
+    }
+}
