0dc4b7c8094bee59e17a793025dcdbdf5dff2eb8
Yusuf Pisan <pisan@uw.edu>
2017-10-30 13:10:39 -0700

    start

---
+++ classexercises/week06/lec1/Lec6p1.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Lec6p1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Lec6p1
+{
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return y;
+    }
+}
