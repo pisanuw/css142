f4b7491479a983baedd82efec195f551da8b8ef2
Yusuf Pisan <pisan@uw.edu>
2017-12-11 16:04:47 -0800

    Did some work, need details.

---
+++ classexercises/week10/SlidePuzzle/Ship.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Ship here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Ship
+{
+    Ship()
+    {
+    }
+    
+    public static void testFleet()
+    {
+        Ship fleet[] = new Ship[5];
+        fleet[0] = new Ship();
+        fleet[1] = new Ship();
+
+        fleet[0] = new Ship();
+        
+    }
+}
---
+++ classexercises/week10/SlidePuzzle/SlidePuzzle.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class SlidePuzzle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SlidePuzzle
+{
+    public static void foo()
+    {
+        int a = 100;
+        System.out.println("Testing");
+        int arr[] = { 1, 2, 3 };
+        System.out.println(arr[3]);
+    }
+}
